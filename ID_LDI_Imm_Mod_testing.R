4215ae68aa19f3345529988b752937b4be1a215e
SarahBassing <sarah.bassing@gmail.com>
2019-03-28 14:00:14 -0700

    Messing with likelihood

--- ID_LDI_Imm_Mod_testing.R
+++ ID_LDI_Imm_Mod_testing.R
@@ -6,6 +6,7 @@
   #  immigrants per pack. This includes a random effects for pack. Data are 
   #  pooled across study areas to increase sample size (also no difference 
   #  between study areas in the proportion of long-distance immigrants per pack).
+  #  P.S. Box 6.2.1 in Hooten & Hobbs 2015 explains this random effect very well.
 ################################################################################
   #  Load packages & data  
   require(R2jags)
@@ -14,8 +15,8 @@
   
   load.module("glm")
   
-  setwd("G:/My Drive/1 University of Montana/Immigration_Hypothesis_Testing/Models")
-  load("./Input/ID_imms.RData")
+  setwd("G:/My Drive/1_Repositories/CoopBreeder_Immigration")
+  load("./Input/ID_imms.RData") 
 
 ################################################################################  
   #  Set up data for model  
@@ -28,14 +29,24 @@
     ungroup()
   nrows <- nrow(packd)
   
+  #  Reformat structure of harvest covariate... 
+  # harv <- packd[,c(1,3,4)] %>%
+  #   tidyr::spread("Year", "Harvest") %>%
+  #   mutate(
+  #     Putative = as.numeric(as.factor(Putative))
+  #   )
+  # yrs <- seq(1,9)
+  # colnames(harv) = c("Putative", yrs)
+  
   win.data <- list(
     "y" = packd$Nimm,
-    "YR" = as.numeric(as.factor(packd$Year)),  # - 2008
+    "YR" = as.numeric(as.factor(packd$Year)),  # packd$Year - 2008
     "AREA" = packd$Region - 1,
-    "HARV" = packd$Harvest, 
+    "HARV" = packd$Harvest,  #harv[,2:10] #model not happy with this version of formatting
     "SRVY" = as.numeric(scale(packd$nSites)),
     "PCK" = as.numeric(as.factor(packd$Putative)),
-    "packd" = as.numeric(packd$Nsampled),
+    "psize" = as.numeric(packd$Nsampled),
+    "nyear" = length(unique(packd$Year)),
     "npack" = length(unique(packd$Putative)), 
     "nrow" = nrows
   )
@@ -47,37 +58,54 @@
   cat("
   model {
     
-    #  Likelihhod with random effect for pack
-    for(i in 1:nrow) {
-      y[i] ~ dbin(p[i], packd[i])
-      for(j in 1:npack) {
-        logit(p[i,j]) <- alpha[j] + B1 * HARV[i]   #alpha[PCK[i]]
-        # now the indexing is all screwed up. need to figure this out.
+    #  Linear predictor with random effect for pack
+    for(pk in 1:npack) {
+      for(i in 1:nrow) {
+        logit(lp[pk,i]) <- alpha[pk] + B1 * HARV[i]
       }
     }
 
+    for(i in 1:nrow) {
+      y[i] ~ dbin(lp[PCK[i],i], psize[i])
+    }
+    # Nested indexing on PCK saves me a for loop
 
-    #  Priors for fixed effects
-    B1 ~ dnorm(0, 0.35)
+    # Various attempts at the likelihood, etc. that aren't working out
 
-    #  Priors for random effect of pack
+    # #  Josh's version but dimensions of HARV don't make sense to me and it doens't work
+    # for(pk in 1:npack) {
+    #   for(yr in 1:nyear) {
+    #     logit(lp[pk, yr]) <- alpha[pk] + B1 * HARV[pk, yr]
+    #   }
+    # }
+    #
+    # #  Josh's version but model doesn't like dimensions of lp
+    # for(i in 1:nrow) {
+    #   y[i] ~ dbin(lp[PCK[i], YR[i]])  #, psize[i]
+    # }
+
+    # #  Original version with likelihood and linear predictor combined in 1 loop:
+    # for(i in 1:nrow) {
+    #   y[i] ~ dbin(lp[i], psize[i])
+    #   logit(lp[i]) <- alpha[PCK[i]] + B1 * HARV[i]
+    # }
+
+
+    ##  Priors
+    #  Fixed effects
+    B1 ~ dnorm(0, 0.35)  # Harvest (Y/N)
+
+    #  Random effect of pack
     for(j in 1:npack) {
       alpha[j] ~ dnorm(0, sd.tau)
     }
-    # #  Priors for random effect of year
-    # for(j in 1:YR) {
-    #   alpha[j] ~ dnorm(0, tau.alpha)
-    # }
 
-    #  Hyperprior for random effect of pack
+    #  Hyperparameters for random effect of pack
     sd.alpha ~ dunif(0, 200)
     sd.tau <- 1/(sd.alpha^2)
-    #tau.alpha ~ dscaled.gamma(sd.tau, df)  # what the heck should the values of these parameters be?!
-    #tau.alpha ~ dgamma(0.01, 0.01) # this works-ish but 95%CRI = 0 - 800 so...
-    #tau.alpha ~ dunif(0, 200) # definitely not working on precision
-    #sd.alpha <- sqrt(1/tau.alpha)
 
-    #  Derived parameters
+
+    ##  Derived parameters
     #  Proportion of immigrants in each pack when harvest did & did not occur
     for(k in 1:2) {
       for(p in 1:npack) {   # also try YR
@@ -86,8 +114,8 @@
     }
 
     #  Mean proportion of immigrants across packs when harvest did & did not occur
-    for(h in 1:2) {
-      mean.imm[h] <- mean(ind.imm[h,])
+    for(k in 1:2) {
+      mean.imm[k] <- mean(ind.imm[k,])
     }
 
       }
@@ -98,16 +126,15 @@
   #  Define initial values and parameters to monitor
   inits <- function(){
     list(
-      B0 = runif(1, -1, 1),
       B1 = runif(1, -1, 1)
     )
   }
   params <- c("B1", "alpha", "sd.tau", "sd.alpha", "mean.imm") #"ind.imm", 
   
   #  MCMC settings
-  ni <- 25000
+  ni <- 50000
   nt <- 1
-  nb <- 20000
+  nb <- 40000
   nc <- 3
   
   #  Call JAGS
@@ -117,7 +144,7 @@
   
   print(out, dig = 2)
   
-  #mcmcplot(out)
+  mcmcplot(out)
   
   
   
